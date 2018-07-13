2807b05738acbdcfe166d9572634771eb8e68482
727030664@qq.com <727030664@qq.com>
2018-07-13 15:10:59 +0800

    测试添加启动图，更改webview背景图片

---
+++ app/src/main/java/com/ansen/webview/LaunchActivity.java
@@ -0,0 +1,34 @@
+package com.ansen.webview;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class LaunchActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            supportRequestWindowFeature(Window.FEATURE_NO_TITLE);//去掉顶部标题栏
+            //加载启动图片
+            setContentView(R.layout.activity_launch);
+            //后台处理耗时任务
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    //耗时任务，比如加载网络数据
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //跳转至 MainActivity
+                            Intent intent = new Intent(LaunchActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            //结束当前的 Activity
+                            LaunchActivity.this.finish();
+                        }
+                    });
+                }
+            }).start();
+        }
+    }
